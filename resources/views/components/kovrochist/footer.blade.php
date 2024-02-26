ca63d361553e7725ef7c75f0d326a1530e58a52b
Andrew <code70@inbox.ru>
2024-02-26 07:35:38 +0300

    asd

--- resources/views/components/kovrochist/footer.blade.php
+++ resources/views/components/kovrochist/footer.blade.php
@@ -2,7 +2,31 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                footer
+                <div class="footer-logo mb-2">
+                    <img src="/kovrochist/img/logo.png" alt="" class="img-fluid">
+                    <span style="line-height: .9">Вятский<br>коврочист</span>
+                </div>
+                <p class="text-light">
+                    Профессиональная стирка и чистка ковров в Кирове с бесплатной доставкой
+                </p>
+            </div>
+            <div class="col"></div>
+            <div class="col">
+                <h5 class="border-bottom pb-2 border-light">Контакты</h5>
+                <ul class="list-unstyled p-0 m-0">
+                    <li class="d-flex align-items-center mb-2">
+                        <i class="fa fa-phone"></i>
+                        <p class="mb-0 mx-2">+7 (901) 479-49-40</p>
+                    </li>
+                    <li class="d-flex align-items-center mb-2">
+                        <i class="fa fa-envelope"></i>
+                        <p class="mb-0 mx-2">covrochistkirov@mail.ru</p>
+                    </li>
+                    <li class="d-flex align-items-center mb-2">
+                        <i class="fa fa-map-marker"></i>
+                        <p class="mb-0 mx-2">Киров, ул.Романа Ердякова, 25</p>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
